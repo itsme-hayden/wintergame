a8fbbae8f154930524a3fc7b2a3819c8e8799246
jbb248 <joebray248@outlook.com>
2025-01-06 13:41:37 -0600

    Finally solve rocket movement interpolation

--- assets/asteroids/scenes/Asteroids.cs
+++ assets/asteroids/scenes/Asteroids.cs
@@ -7,17 +7,19 @@ public partial class Asteroids : MicroGame
 
 	public const double MeteorSpawnTime = 1.0;
 
-	private Area2D _rocket;
+	private Rocket _rocket;
+	private RigidBody2D _followPoint;
 	private PackedScene _meteorScene;
 
 	private int _meteorCount = 5;
 	private double _meteorTimer = MeteorSpawnTime;
 
 	public override void _Ready() 
-	{ 
+	{
 		base._Ready();
 
-		_rocket = GetNode<Area2D>("Rocket");
+		_rocket = GetNode<Rocket>("Rocket");
+		_followPoint = GetNode<RigidBody2D>("FollowPoint");
 		_meteorScene = GD.Load<PackedScene>("res://assets/asteroids/scenes/meteor.tscn");
 
 		// WHY ARE SPRITES CENTER ANCHORED?
@@ -25,6 +27,8 @@ public partial class Asteroids : MicroGame
 		// This is going to create so many problems for me T.T
 		var viewport = GetViewport().GetVisibleRect();
 		_rocket.Position = new Vector2(viewport.Size.X / 2, viewport.Size.Y / 2);
+		_followPoint.Position = _rocket.Position;
+		_followPoint.LinearVelocity = new Vector2(0, -150);
 	}
 
 	public override void _Process(double delta) 
@@ -43,12 +47,40 @@ public partial class Asteroids : MicroGame
 				meteor.Rotation = spawn.Rotation;
 				meteor.MoveTowardCenter(_rocket.Position);
 				meteor.AngularVelocity = MeteorVelocities[GD.Randi() % MeteorVelocities.Length] * 16;
-				AddChild(meteor);
+				// AddChild(meteor);
 			}
 		}
 	}
 
-	public void OnRocketHit(Node2D body)
+    public override void _Input(InputEvent _event)
+    {
+        base._Input(_event);
+
+		// These will need to be touch events in the future
+		if(_event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left && mouseEvent.IsReleased())
+		{
+
+			if(mouseEvent.Position.X < GameUtil.ViewportDimensions.X / 2)
+			{
+				_followPoint.LinearVelocity = _followPoint.LinearVelocity.Rotated(-MathF.PI / 8);
+				_followPoint.Rotate(-MathF.PI / 8);
+				GD.Print("Rotate CCW");
+			}
+			else
+			{
+				_followPoint.LinearVelocity = _followPoint.LinearVelocity.Rotated(MathF.PI / 8);
+				_followPoint.Rotate(MathF.PI / 8);
+				GD.Print("Rotate CW");
+			}
+		}
+    }
+
+	private void UpdateFollowPoint()
+	{
+		
+	}
+
+    public void OnRocketHit(Node2D body)
 	{
 		GD.Print("GAMEOVER");
 		// EndMicroGame();
